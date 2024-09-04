75f931fd35baf5064fb94ce45c0e1de08f08d017
Eric Leow <ericleowyuquan@gmail.com>
2024-09-04 16:32:54 +0800

    Preliminary base react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo-container">
+        <div className="logo-icon">Code Editor</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
